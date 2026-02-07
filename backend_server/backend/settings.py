d74b6e20587945c48a79753e7fda9e4b06d0ed86
Balram Kumar Pandey <kpbalram810@gmail.com>
2026-02-07 12:34:34 +0530

    Add custom domain to allowed hosts

--- backend_server/backend/settings.py
+++ backend_server/backend/settings.py
@@ -32,8 +32,13 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'False'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
-
+ALLOWED_HOSTS = [
+    'apna-room-fz6ml.ondigitalocean.app', 
+    'www.apnaroom.co.in',
+    'apnaroom.co.in',
+    'localhost', 
+    '127.0.0.1'
+]
 
 # Application definition
 
@@ -107,8 +112,20 @@ if database_url:
     except Exception as e:
         print(f"Warning: DATABASE_URL detected but failed to load: {e}")
 
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://apna-room-fz6ml.ondigitalocean.app',
+    'https://www.apnaroom.co.in',
+    'https://apnaroom.co.in',
+]
+
 # CORS SETTINGS
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "https://apna-room-fz6ml.ondigitalocean.app",
+    "https://www.apnaroom.co.in",
+    "https://apnaroom.co.in",
+]
 
 
 # Password validation
