22a32c44bc9d7883bfb6a99f8cf631161862f0b7
Balram Kumar Pandey <kpbalram810@gmail.com>
2026-02-13 18:08:31 +0530

    add UUID

--- backend_server/api/models.py
+++ backend_server/api/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+import uuid
 
 class Colony(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
